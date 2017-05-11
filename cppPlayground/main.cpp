2cb6a90d544b0245dcff2c5068af8f3c772b1a84
Muhsin Fatih Yorulmaz <muhsinfatih@gmail.com>
2017-05-11 15:07:24 +0300

    Initial Commit

---
+++ cppPlayground/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cppPlayground
+//
+//  Created by Muhsin Fatih Yorulmaz on 11/05/2017.
+//  Copyright © 2017 mekatrotekno. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
